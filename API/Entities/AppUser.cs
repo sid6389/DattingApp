a3f003f27f1cd0412efabfcd2ee3516710966f7c
sid <patel.siddharth.m@gmail.com>
2021-02-28 21:53:32 -0500

    Angular Setup

--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -5,7 +5,9 @@ namespace API.Entities
         public int Id { get; set; }
         public string UserName { get; set; }      
         
-        
+        public byte[] Password { get; set; }
+
+        public byte[] PasswordSalt { get; set; }
         
     }
 }
